18bb1edb09af085d026177c2437e1af5a725e883
Krygore <webdev.krygore@gmail.com>
2022-10-20 21:34:22 -0700

    Completed Task 2 of MVP

--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+  const StyledDiv = styled.div`
+    background: lightgrey;
+  `;
+  const StyledH3 = styled.h3`
+  color: darkslategrey;
+  border-bottom: dotted black 3px;
+  `;
+  const StyledP = styled.p`
+  border: solid black 2px;
+  border-radius: 20px;
+  margin: 5px 10px;
+  padding: 5px 10px;
+  background: darkslategrey;
+  color: white;
+  `;
+  const StyledImg = styled.img`
+  border: solid black 3px;
+  border-radius: 10px;
+  
+  
+  &:hover {
+    transform: scale(1.3);
+  }
+  `;
 
   const NasaPhoto = (props) => {
     return (
-        <div className='nasa-photo-wrapper'>
-            <h3>{props.photo.title}</h3>
-            <p>{props.photo.date}</p>
-            <img src={props.photo.hdurl} alt={props.photo.title} />
-            <p className='explanation'>{props.photo.explanation}</p>
-        </div>
+        <StyledDiv className='nasa-photo-wrapper'>
+            <StyledH3>{props.photo.title}</StyledH3>
+            <StyledP>{props.photo.date}</StyledP>
+            <StyledImg src={props.photo.hdurl} alt={props.photo.title} />
+            <StyledP className='explanation'>{props.photo.explanation}</StyledP>
+        </StyledDiv>
     )
   }
 
